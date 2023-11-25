40a61a6a5bb8b386bc670cab17c9c5dd2b3bf257
Piotr Merecki <yamakiteo@gmail.com>
2023-11-25 20:13:33 +0100

    Zmiany 22

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -3,7 +3,7 @@ package com.kodilla;
 public class Calculator {
     public void someMethod() {
         System.out.println("It works!");
-        System.out.println("Very Well!!!");
+        System.out.println("Very Well!!!!!");
     }
 
     public static void main(String[] args) {
